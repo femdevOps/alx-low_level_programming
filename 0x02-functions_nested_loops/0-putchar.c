a6322c927e3f58cb8746fdbde36cd6fcd7d43f88
femdevOps <oluwatosinpeters@outlook.com>
2022-09-14 08:09:47 -0700

    Starting functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_puchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This directory contains alx functions assignment or project.
+
