5e46f312257d9cb079f485311f3cff4852f50b86
femdevOps <oluwatosinpeters@outlook.com>
2022-12-12 13:46:31 -0800

    c process

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: print all alphabets
+ * Description: prints sizes of data type
  * Return: 0
  */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+      int intType;
+      float floatType;
+      double doubleType;
+      char charType;
+      
+      printf("Size of a int: %zu bytes\n", sizeof(intType));
+      printf("Size of a float: %zu bytes\n", sizeof(floatType));
+      printf("Size of a double: %zu bytes\n", sizeof(doubleType));
+      printf("Size of a char: %zu byte\n", sizeof(charType));
+      return (0);
 }
 
