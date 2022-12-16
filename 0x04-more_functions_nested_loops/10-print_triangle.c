4b34dc3132c8113478f4fe7573be3b38ce2afe71
femdevOps <oluwatosinpeters@outlook.com>
2022-12-15 20:10:15 -0800

    done

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,17 +13,22 @@ void print_triangle(int size)
 	int i, j, spaces;
 
 	if (size <= 0)
-		_putchar('\n');
-
-	for (i = 1; i <= size; i++)
 	{
-		for (spaces = size - i; spaces >= 1; spaces--)
-		{
-			_putchar(' ');
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (spaces = size - i; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,21 +3,20 @@
 /**
  * more_numbers - Prints numbers from 0 to 14, ten times
  *
- * Return: no return 
+ * Return: no return
  *
  */
 
 void more_numbers(void)
 {
-
-int i, j;
+	int i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (num >= 10)
-			_putchar('1');
+				_putchar('1');
 			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
